d957f6127083dd2b371044e5b0a6499abacfa765
Oscar Domingo <oscardomingo@gmail.com>
2020-02-26 17:53:45 +0000

    Refactored the whole extension.js
    I shamelessly copied the "Places" extension and butchered
    it until I got what I needed.
    
    Also made it compatible with 3.34.

--- extension.js
+++ extension.js
@@ -1,24 +1,70 @@
-imports.gi.versions.St = "1.0";
+/* -*- mode: js2; js2-basic-offset: 4; indent-tabs-mode: nil -*- */
+/* exported init enable disable */
 
-const St = imports.gi.St;
-const Lang = imports.lang;
+const { GObject, St } = imports.gi;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
-const Util = imports.misc.util;
-const Main = imports.ui.main;
+
 const GLib = imports.gi.GLib;
+const Util = imports.misc.util;
 
+
+var PlaceMenuItem = GObject.registerClass(
+class PlaceMenuItem extends PopupMenu.PopupBaseMenuItem {
+    _init(info) {
+        super._init();
+        this._info = info;
+
+        this._icon = new St.Icon({
+            gicon: info.icon,
+            icon_size: 16,
+        });
+        this.add_child(this._icon);
+
+        this._label = new St.Label({ text: info.name, x_expand: true });
+        this.add_child(this._label);
+
+        this._changedId = info.connect('changed',
+            this._propertiesChanged.bind(this));
+    }
+
+    destroy() {
+        if (this._changedId) {
+            this._info.disconnect(this._changedId);
+            this._changedId = 0;
+        }
+
+        super.destroy();
+    }
+
+    activate(event) {
+        this._info.launch(event.get_time());
+
+        super.activate(event);
+    }
+
+    _propertiesChanged(info) {
+        this._icon.gicon = info.icon;
+        this._label.text = info.name;
+    }
+});
+
+
+let VarietyMenu = GObject.registerClass(
 class VarietyMenu extends PanelMenu.Button{
-    constructor(){
-        super(St.Align.START);
+    _init() {
+        super._init(0.0);
 
-        let box = new St.BoxLayout();
+        let hbox = new St.BoxLayout();
 
         let icon = new St.Icon({ icon_name: 'variety-indicator',
            style_class: 'system-status-icon' });
-        box.add(icon);
 
-        this.actor.add_child(box);
+        hbox.add_child(icon);
+        this.add_actor(hbox);
 
         //Pause
         let playback_pause = new PopupMenu.PopupImageMenuItem('Pause', 'media-playback-pause-symbolic');
@@ -26,12 +72,14 @@ class VarietyMenu extends PanelMenu.Button{
             Util.spawn(['variety', '--pause']);
         });
         this.menu.addMenuItem(playback_pause);
+
         //next
         let playback_resume = new PopupMenu.PopupImageMenuItem('Resume', 'media-playback-start-symbolic');
         playback_resume.connect('activate',  ()=>{
             Util.spawn(['variety', '--resume']);
         });
         this.menu.addMenuItem(playback_resume);
+
         //next
         let playback_next = new PopupMenu.PopupImageMenuItem('Next', 'go-next-symbolic');
         playback_next.connect('activate',  ()=>{
@@ -44,73 +92,74 @@ class VarietyMenu extends PanelMenu.Button{
             Util.spawn(['variety', '--previous']);
         });
         this.menu.addMenuItem(playback_previous);
+        
+        let show_image = new PopupMenu.PopupImageMenuItem('Open Image', 'open-menu-symbolic');
+        this.menu.addMenuItem(show_image);
 
-        //image-viewer
-        // let show_image = new PopupMenu.PopupImageMenuItem('View Image', 'open-menu-symbolic');
-        // show_image.connect('activate',  ()=>{
-        //     Util.spawn(['variety', '--pause']);
-        // });
-        // this.menu.addMenuItem(show_image);
-
-        //view at source
+        // View at source (needs CLI option for this).
         // let show_source = new PopupMenu.PopupImageMenuItem('View at source', 'link-symbolic');
         // show_source.connect('activate',  ()=>{
         //     Util.spawn(['variety', '--pause']);
         // });
         // this.menu.addMenuItem(show_source);
 
-        //copy to fav
+        // Copy the current Wallpaper to the favourites folder.
         let copy_to_favourites = new PopupMenu.PopupImageMenuItem('Copy to Favourites', 'starred-symbolic');
         copy_to_favourites.connect('activate',  ()=>{
             Util.spawn(['variety', '--favorite']);
         });
         this.menu.addMenuItem(copy_to_favourites);
-        // delete to trash
+
+        // Delete (and blacklist) the current Wallpaper.
         let delete_to_trash = new PopupMenu.PopupImageMenuItem('Delete to Trash', 'edit-delete-symbolic');
         delete_to_trash.connect('activate',  ()=>{
             Util.spawn(['variety', '--trash']);
         });
         this.menu.addMenuItem(delete_to_trash);
-        // image submenu
+
 
         // Separator 
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
         // Toggle history
-        let show_history = new PopupMenu.PopupMenuItem('History');
+        let show_history = new PopupMenu.PopupMenuItem('Toggle History display');
         show_history.connect('activate',  ()=>{
             Util.spawn(['variety', '--history']);
         });
         this.menu.addMenuItem(show_history);
 
-        // Wallpaper selection
-        let wallpaper_selector = new PopupMenu.PopupMenuItem('Wallpaper Selector');
+        // Toggle Wallpaper selection display
+        let wallpaper_selector = new PopupMenu.PopupMenuItem('Toggle Wallpaper Selector display.');
         wallpaper_selector.connect('activate',  ()=>{
             Util.spawn(['variety', '--selector']);
         });
         this.menu.addMenuItem(wallpaper_selector);
-        // Recent Downloads
-        // let recent_downloads = new PopupMenu.PopupMenuItem('Recent Downloads');
-        // recent_downloads.connect('activate',  ()=>{
-        //     Util.spawn(['variety', '--downloads']);
-        // });
-        // this.menu.addMenuItem(recent_downloads);
+
+        // Toggle Recent Downloads Display
+        let recent_downloads = new PopupMenu.PopupMenuItem('Toggle Recent Downloads display.');
+        recent_downloads.connect('activate',  ()=>{
+            Util.spawn(['variety', '--downloads']);
+        });
+        this.menu.addMenuItem(recent_downloads);
+
         // Preferences
         let preferences = new PopupMenu.PopupMenuItem('Preferences...');
         preferences.connect('activate',  ()=>{
             Util.spawn(['variety', '--preferences']);
         });
         this.menu.addMenuItem(preferences);
+
         // About
-        // let about = new PopupMenu.PopupMenuItem('About...');
-        // about.connect('activate',  ()=>{
-        //     Util.spawn(['variety', '--pause']);
-        // });
-        // this.menu.addMenuItem(about);
+        let about = new PopupMenu.PopupMenuItem('About...');
+        about.connect('activate',  ()=>{
+            Util.spawn(['gio', 'open', 'https://peterlevi.com/variety/']);
+        });
+        this.menu.addMenuItem(about);
+
         // Donate
         let donate = new PopupMenu.PopupMenuItem('Donate...');
         donate.connect('activate',  ()=>{
-            Util.spawn(['xdg-open', 'https://peterlevi.com/variety/donate/']);
+            Util.spawn(['gio', 'open', 'https://peterlevi.com/variety/donate/']);
         });
         this.menu.addMenuItem(donate);
         //Quit
@@ -121,20 +170,49 @@ class VarietyMenu extends PanelMenu.Button{
         this.menu.addMenuItem(quit);
 
 
+        this.menu.connect('open-state-changed', ()=>{
+            if(this.menu.isOpen) {
+                this._update_wallpaper_path();
+            };
+
+        });
 
     }
+
+    _update_wallpaper_path() {
+        if (this._open_image_action_id) {
+            this.menu.box.get_children()[4].disconnect(this._open_image_action_id);    
+        }
+
+        let wp_path = ''
+        let [get_wp_success, get_wp_stdout, get_wp_stderr, get_wp_exit_status] = GLib.spawn_command_line_sync('variety --get');
+
+        wp_path = imports.byteArray.toString(get_wp_stdout).replace("Variety is already running. Sending the command to the running instance.", "");
+        wp_path = wp_path.trim();
+
+        this._open_image_action_id = this.menu.box.get_children()[4].connect('activate',  ()=>{
+            Util.spawn(['gio', 'open', wp_path]);
+        });
+    }
+
+    _onDestroy() {
+        super._onDestroy();
+    }
+
+});
+
+
+function init() {
+    //ExtensionUtils.initTranslations();
 }
 
-let varietyMenu;
+let _indicator;
 
-function init(){
-}
-
-function enable(){
-    varietyMenu = new VarietyMenu();
-    Main.panel.addToStatusArea('VarietyMenu', varietyMenu, 0, 'right');
+function enable() {
+    _indicator = new VarietyMenu();
+    Main.panel.addToStatusArea('places-menu', _indicator, 0, 'right');
 }
 
 function disable() {
-    varietyMenu();
+    _indicator.destroy();
 }
